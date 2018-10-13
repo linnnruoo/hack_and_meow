99af149f1533dac8dbcd7ff46a48a9d246544142
QzSG <quantumparadoxes@gmail.com>
2018-10-13 21:13:14 +0800

    Add code to inteface with CV Api
    Check test input component for sample usage.
    
    retrieveTags return raw Json response.
    isCat does what it does :cat:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import TestInput from './TestInput'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <TestInput />
       </div>
     );
   }
