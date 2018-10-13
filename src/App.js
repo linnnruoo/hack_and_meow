50f1eee894563db0fb954d38a34e4c1bca777b4f
linnnruoo <linruo2014@gmail.com>
2018-10-13 21:22:05 +0800

    add pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Upload from './components/Upload';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: '"Mali"'
+  }
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <div className="App">
+            <div className="children">
+              <Route exact path="/" component={ Home } />
+              <Route exact path="/upload" component={ Upload } />
+            </div>
+          </div>
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
